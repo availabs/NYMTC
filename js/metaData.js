9f93cb6ff6e4a2592b5032f78b49b0b8f939746b
Alex <am3081@gmail.com>
2016-03-23 13:41:41 -0400

    smal lchanges

--- js/metaData.js
+++ js/metaData.js
@@ -1,66 +1,66 @@
 var RadarMethods = [
-		'Origin Location',
-		'Destination Location',
-		'Trip Purpose',
-		'Mode',
-		'Parking Info',
-		'Toll Info',
-		'Route Info',
-		'Departure Time',
-		'Arrival Time',
-		'Travel Party',
-		'Respondent Burden Reduction',
-		'Customizable',
-		'Data Processing',
-		'Equity',
-		'Commercial Availability',
-		'Survey Instrument Lifecycle',
-		'Administrative Features',
-		'Respondent Recruitment',
-		'Historical Comparison',
-		'Demographic Data'
-	]
+	'Origin Location',
+	'Destination Location',
+	'Trip Purpose',
+	'Mode',
+	'Parking Info',
+	'Toll Info',
+	'Route Info',
+	'Departure Time',
+	'Arrival Time',
+	'Travel Party',
+	'Respondent Burden Reduction',
+	'Customizable',
+	'Data Processing',
+	'Equity',
+	'Commercial Availability',
+	'Survey Instrument Lifecycle',
+	'Administrative Features',
+	'Respondent Recruitment',
+	'Historical Comparison',
+	'Demographic Data'
+]
 
-	var methodCategories = {
-		'Trip Characteristic Data': {
-			color: '#3faecd',
-			methods : [
-				'Origin Location',
-				'Destination Location',
-				'Trip Purpose',
-				'Mode',
-				'Parking Info',
-				'Toll Info',
-				'Route Info',
-				'Departure Time',
-				'Arrival Time',
-				'Travel Party'
-			]
-		},
-		
-		'Demographic Data':{
-			color: '#89bf3f',
-			methods: ['Demographic Data']
-		},
+var methodCategories = {
+	'Trip Characteristic Data': {
+		color: '#3faecd',
+		methods : [
+			'Origin Location',
+			'Destination Location',
+			'Trip Purpose',
+			'Mode',
+			'Parking Info',
+			'Toll Info',
+			'Route Info',
+			'Departure Time',
+			'Arrival Time',
+			'Travel Party'
+		]
+	},
+	
+	'Demographic Data':{
+		color: '#89bf3f',
+		methods: ['Demographic Data']
+	},
 
-		'Survey Instrument Characteristics':{
-			color: '#e65e38',
-			methods: [
-				'Respondent Burden Reduction',
-				'Customizable',
-				'Data Processing',
-				'Equity',
-				'Commercial Availability',
-				'Survey Instrument Lifecycle'
-			]
-		},
+	'Survey Instrument Characteristics':{
+		color: '#e65e38',
+		methods: [
+			'Respondent Burden Reduction',
+			'Customizable',
+			'Data Processing',
+			'Equity',
+			'Commercial Availability',
+			'Survey Instrument Lifecycle'
+		]
+	},
 
-		'Admin Characteristics': {
-			color: '#e9c659',
-			methods: [
-				'Administrative Features',
-				'Respondent Recruitment',
-				'Historical Comparison',
-			]
-		}
+	'Admin Characteristics': {
+		color: '#e9c659',
+		methods: [
+			'Administrative Features',
+			'Respondent Recruitment',
+			'Historical Comparison',
+		]
 	}
+}
